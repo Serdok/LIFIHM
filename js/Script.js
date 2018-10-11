c4d1a4160adf6914affd1dfd836adfc57df7a1dc
Serdok <l.anass.pro@gmail.com>
2018-10-11 09:44:43 +0200

    Push 1

---
+++ js/Script.js
@@ -0,0 +1,29 @@
+var menu = document.querySelector( "nav" );
+var tableau = document.querySelector( "div#tableau" );
+
+
+function repli () // Replie le tableau
+{
+	
+};
+
+function remove (line) // Supprime une ligne
+{
+
+	alert( "Ligne " + line + " supprimée!" );
+};
+
+function add () // Ajoute une ligne à la fin du tableau
+{
+	
+};
+
+function change (line) // Modifie une ligne du tableau
+{
+	
+};
+
+function research () // Recherche un lieu précis
+{
+	
+};
